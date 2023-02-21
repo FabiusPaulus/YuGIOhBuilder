9f14c1e4755f40f8061e11428afa2168c0020f66
FabiusPaulus <benthschneiderfabian@gmail.com>
2023-02-21 14:26:39 +0100

    not done server connection initial login page

---
+++ main.js
@@ -0,0 +1,13 @@
+var userNameInputSave;
+var passwordInputSave;
+
+function visitLogin()
+{
+    window.location = 'login.html';
+}
+
+function getLoginData()
+{
+    userNameInputSave = document.getElementById("userNameInput").value;
+    passwordInputSave = document.getElementById("passwordInput").value;
+}
